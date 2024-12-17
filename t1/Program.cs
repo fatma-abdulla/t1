8550dfcf4788d36f0a1afd5f0195fbc74179fb2a
fa6om <fa6om@LAPTOP-4OBBIE72>
2024-12-17 19:35:10 +0300

    Add project files.

---
+++ t1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+int id = 332;
+double gread = 99.9;
+string name = "fatma";
+bool female = true;
+char mychar = 'A';
+
+Console.WriteLine("your id is ; " + id);
+Console.WriteLine("your gread is; " + gread);
+Console.WriteLine("your name is ; " + name);
+Console.WriteLine("your gender is female ; " + female);
+
+Console.WriteLine("----------------------------------------------------");
+
+int number = 5;
+double x = Convert.ToDouble(number);
+
+Console.WriteLine("the value is : " + number);
+Console.WriteLine("the double is : " + x);
+Console.WriteLine("the char is" + mychar);
+
+
+Console.WriteLine("----------------------------------------------------");
+
+char[] charArray = { 'H', 'e', 'l', 'l', 'o' };
+Console.WriteLine("the word is ; ");
+foreach (char c in charArray){
+    Console.WriteLine(c);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
